6acff8b79400f3b4cd9b777a8cda9fb71270469b
SAMUEL15758 <samuelgordo787@gmail.com>
2025-03-07 16:54:53 -0500

    Add files via upload

---
+++ bucle.py
@@ -0,0 +1,35 @@
+contracorrecta = 123
+
+while True:
+    contraseña = int(input("ingrese la contraseña: "))
+    if (contraseña == contracorrecta):
+        print("contraseña correcta")
+        break
+    else:
+        print("contraseña incorrecta")
+
+
+
+for i in range(2, 21, 2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
